cab62a691c2c18925457b41819882868c6b95247
Your Name <you@example.com>
2026-01-16 17:08:54 +0530

    Start Python tutorial. First commit Add initial Python script with variable naming conventions and data types examples,

---
+++ main.py
@@ -0,0 +1,68 @@
+print("Hello, World!")
+
+#comment in python
+#hey i am saurabh //Single lin comment
+"""Hii saurabh this is 
+multi line comment
+"""
+
+#variable in python
+#variable name should not start with number
+#variable name should not contain special character except e.g.: @,$,!,%,^,&,*
+#variable name should not be a keyword
+
+sher ="harsh fournder of sheryiian ai school"
+a=12
+
+# Naming convenstions in python three ways to write variable names
+# 1.camel case
+myNameIsSaurabh = "saurabh"
+# 2.pascal case
+MyNameIsSaurabh = "saurabh"
+# 3.snake case
+my_name_is_saurabh = "saurabh"
+#data types in python
+#1.string
+name = "saurabh"
+
+#Three type of number data type in python
+#1.complex, 2.integer, 3.float
+#1.complex
+complex_number = 3 + 4j
+
+#2.integer
+age = 21
+#3.float
+height = 5.9
+#p/q this formet is called as rational number that is also called as fraction or floating point number
+c=12/4
+
+#4.boolean
+is_student = True
+is_student=False
+#5.list
+fruits = ["apple", "banana", "cherry"]
+#6.tuple
+coordinates = (10.0, 20.0)
+#7.dictionary
+person = {"name": "saurabh", "age": 21}
+#8.set
+unique_numbers = {1, 2, 3, 4, 5}
+#type function
+print(type(name))
+print(type(complex_number))
+print(type(age))
+print(type(height))
+print(type(c))
+print(type(is_student))
+print(type(fruits))
+print(type(coordinates))
+print(type(person))
+print(type(unique_numbers))
+#input function
+
+# String type conversion
+a = "A"
+print(ord(a))  # Output: 65, ASCII value of 'A', converts character to integer using ord function
+b=65
+print(chr(b))  # Output: 'A', converts integer to character using chr function
