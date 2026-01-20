2fb0b705701963f049fdc9cc2467bfc1e0261e1e
saurabh11837 <patelsaurabhroa@gmail.com>
2026-01-20 14:21:51 +0530

    Some comcept of DSA, like "List","Tuple", "Set", "Dictionary"...

---
+++ pythonDataStructure.py
@@ -0,0 +1,25 @@
+# DataStructure in Python..
+# Data structure ek way hota hai data ko organize, store aur access karne ka.
+# Python me built-in aur custom data structures dono use kar sakte ho.
+
+# | Type           | Examples         | Notes                                                                  |
+# | -------------- | ---------------- | ---------------------------------------------------------------------- |
+# | **List**       | `[1,2,3]`        | Ordered, mutable (changeable), allows duplicates                       |
+# | **Tuple**      | `(1,2,3)`        | Ordered, immutable, allows duplicates                                  |
+# | **Set**        | `{1,2,3}`        | Unordered, mutable, no duplicates                                      |
+# | **Dictionary** | `{'a':1, 'b':2}` | Key-Value pairs, unordered (Python 3.7+ ordered by insertion), mutable |
+
+
+
+# -->Data structures are used to store, organize, and maniupulate data efficiently. Python provides several built-in data structures.
+# -->And for storing multiple values we will again use vaiables.
+# --> Now in python we have 4 type of in-bulld data strucute, "List", "Tuple","Dictionary", "Set".
+
+# Custom data structures
+# 1.Now there are som custom data strucures as well like Stack, Queue, Linked List, Graph et.
+# 2.And around these data strucutes there are some algorithms like searching alogorithms, sortng alogorithms.
+# 3.And this is why the study is called data strucutes and alogorightm.
+
+# 
+
+
